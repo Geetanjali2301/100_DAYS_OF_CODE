8afb518eaf4acb8ef07fed0c4bae8f76157524ba
Geetanjali2301 <geetanjalipohare@gmail.com>
2024-02-22 21:02:12 +0530

    Day 3 edited

---
+++ DAY_4/majority_Element.java
@@ -0,0 +1,15 @@
+package DAY_4;
+import java.util.*;
+/*
+# Leetcode 169
+*/
+
+public class majority_Element {
+    public static void main(String[] args) {
+        int arr[]={3,3,6,7,3,3,3,3};
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length/2]);
+
+    }
+    
+}
