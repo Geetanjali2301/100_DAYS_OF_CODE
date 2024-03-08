dd850c099d636e9360a714353c6c0eca8927ebcf
Geetanjali2301 <geetanjalipohare@gmail.com>
2024-03-08 13:11:57 +0530

    Day 19 edited

---
+++ DAY_19/diagonalSum.java
@@ -0,0 +1,33 @@
+package DAY_19;
+
+public class diagonalSum {
+
+    public static int diagonal(int[][] mat) {
+
+        int sum=0;
+        for(int i=0;i<mat.length;i++)
+        {
+            for(int j=0;j<mat.length;j++)
+            {
+                if(i==j)
+                {
+                    sum+=mat[i][j];
+                }
+                else if(i+j==(mat.length-1) && i!=j )
+                {
+                    sum+=mat[i][j];
+
+                }
+            }
+        }
+
+        return sum;   
+    }
+
+    public static void main(String[] args) {
+        int mat[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int ans=diagonal(mat);
+        System.out.println(ans);
+    }
+    
+}
